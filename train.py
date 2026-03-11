7fdd821d593a4fdc94ba20c7144108e223964abc
royshreya202-rgb <royshreya202@gmail.com>
2026-03-11 13:35:17 +0530

    Fixed code errors

--- train.py
+++ train.py
@@ -1,23 +1,25 @@
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClaasifier
+from sklearn.ensemble import RandomForestClassifier
 import joblib
 
 def train_model():
-    data=load_iris()
-    x=data.data
-    y=data.target
+    data = load_iris()
+    x = data.data
+    y = data.target
 
-    x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
-    model=RandomForestClassifier()
-    model.fit(x_train,y_train)
-    accuracy=mode4l.score(x_test,y_test)
-    print("Model Accuracy:",accuracy)
+    model = RandomForestClassifier()
+    model.fit(x_train, y_train)
 
-    joblib.dump(model,"model.pkl")
+    accuracy = model.score(x_test, y_test)
+    print("Model Accuracy:", accuracy)
+
+    joblib.dump(model, "model.pkl")
     print("Model saved as model.pkl")
 
-    if _name__=="_main_":
-        print("Training started again...")
-        train_model()
+
+if __name__ == "__main__":
+    print("Training started...")
+    train_model()
