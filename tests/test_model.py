5d625f229a4cd0681690092bf8faff7378b9cd7d
royshreya202-rgb <royshreya202@gmail.com>
2026-03-11 13:40:13 +0530

    Final fix

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,2 +1,4 @@
-import osdef test_model_exists():
-assert os.path.exists("model.pkl"),"Model file not found"
+import os
+
+def test_model_exists():
+    assert os.path.exists("model.pkl"), "Model file not found"
--- train.py
+++ train.py
@@ -5,15 +5,17 @@ import joblib
 
 def train_model():
     data = load_iris()
-    x = data.data
+    X = data.data
     y = data.target
 
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
 
     model = RandomForestClassifier()
-    model.fit(x_train, y_train)
+    model.fit(X_train, y_train)
 
-    accuracy = model.score(x_test, y_test)
+    accuracy = model.score(X_test, y_test)
     print("Model Accuracy:", accuracy)
 
     joblib.dump(model, "model.pkl")
